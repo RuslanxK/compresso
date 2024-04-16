9883a02f5825bb5ac397bb0fca8689bde8aa41eb
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-04-16 21:43:36 +0300

    sad

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-var bodyParser = require("body-parser");
 const postRouter = require("./routers/postRouter")
 
 const PORT = 8080;
@@ -8,25 +7,6 @@ const PORT = 8080;
 const app = express();
 
 app.use(cors());
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
-});
-
-var jsonParser = bodyParser.json({
-  limit: 1024 * 1024 * 20,
-  type: "application/json",
-});
-var urlencodedParser = bodyParser.urlencoded({
-  extended: true,
-  limit: 1024 * 1024 * 20,
-  type: "application/x-www-form-urlencoded",
-});
-
-app.use(jsonParser);
-app.use(urlencodedParser);
-
 app.use(express.json());
 
 require("./configs/database");
