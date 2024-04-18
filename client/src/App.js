04b51e749766d1e455480a873c885752870ad44e
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-04-18 10:37:48 +0300

    asd

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import Main from './Main'
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate} from "react-router-dom";
 import Posts from './Posts';
 import PostInner from "./PostInner"
 
 const App = () => {
 
-
+ const navigate = useNavigate()
 
   return (
     <div>
 
-       <img id="logo" src={process.env.PUBLIC_URL + '/logo.png'} alt="logo" width="150px"/>
+       <img id="logo" src={process.env.PUBLIC_URL + '/logo.png'} alt="logo" width="150px" onClick={() => navigate("/")}/>
 
        <Routes>
         <Route path="/" element={<Main />} />
