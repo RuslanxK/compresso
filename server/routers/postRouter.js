f13464dbf6467d601339c4d88f0d463126d04cda
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-04-23 17:23:10 +0300

    asdas

--- server/routers/postRouter.js
+++ server/routers/postRouter.js
@@ -70,7 +70,7 @@ router.post("/posts", upload.single("image"), async (req, res) => {
     res.status(200).json(response);
   } catch (error) {
     console.log(error);
-    res.status(500).json(error);
+    res.status(500).json(error.message);
   }
 });
 
